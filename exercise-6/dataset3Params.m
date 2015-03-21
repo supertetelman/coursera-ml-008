8ed3b3281b10e5da2747615427f753149e433c4c
Adam Tetelman <supertetelman@gmail.com>
2015-03-21 18:11:19 -0400

    Complete part 1 and 2 of exercise 6 - gaussian Kernel

--- exercise-6/dataset3Params.m
+++ exercise-6/dataset3Params.m
@@ -1,34 +1,33 @@
+%ml-008 Exercise6
+%Based off of sample code provided by coursera Machine Learning Course
+%ml-008 taught by Andrew NG of Stanford
+%@author Adam Tetelman 3/21/2015
 function [C, sigma] = dataset3Params(X, y, Xval, yval)
-%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
-%where you select the optimal (C, sigma) learning parameters to use for SVM
-%with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
-%   sigma based on a cross-validation set.
-%
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+%TODO auto populate options arrays via min/max/inc
+%C_options = [0 1 2 4 8 16 32 64 128 256 512 1024 2048];
+%sigma_options = [0 .1 .2 .4 .8 .16 .32 .64 1.28 2.56 5.12 10.24 20.48];
+C_options = [.1 .5 1 5 10 50];
+sigma_options = [.1 .5 1 5 10 50];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+error = ones(length(C_options), length(sigma_options));
 
+%TODO: Improve these x1,x2 values
+x1 = [1 2 1]; x2 = [0 4 -1];
 
+for i=1:length(C_options)
+    for j=1:length(sigma_options)
+        model = svmTrain(X,y,C_options(i),...
+            @(x1, x2) gaussianKernel(x1, x2,sigma_options(j)),1e-3, 20);
+        predictions = svmPredict(model, Xval);
+        error(i,j) = mean(double(predictions ~= yval));
+    end
+end
 
-
-
-
-
-% =========================================================================
+%Return the sigma/C combination with the smallest error
+[a x] = min(error);
+[b d] = min(a);
+C = C_options(x(d));
+sigma = sigma_options(d);
 
 end
